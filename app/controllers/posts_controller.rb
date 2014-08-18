21f28f364e95563c5946c96d2fa68d99d9fb1ee2
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-08-18 17:11:02 +0100

    Creating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,18 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post].permit(:title))
+		@post.save
+
+		redirect_to '/posts'
 	end
 
 end
--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -1,10 +1,37 @@
 require 'rails_helper'
 
 describe 'posts' do
+
 	context 'no posts' do
 	  it 'shows a message' do
 	  	visit '/posts'
 	  	expect(page).to have_content 'No posts yet'
 	  end
 	end
+
+	context 'with posts' do
+		before do
+			Post.create(title: 'Hello World')
+		end
+
+		it 'displays a list of all posts' do
+			visit '/posts'
+			expect(page).to have_content 'Hello World'
+		end
+	end
+
+	describe 'creating posts' do
+
+	  it 'adds the post from a form' do
+	  	visit '/posts'
+	  	click_link 'New Post'
+	  	fill_in 'Title', with: 'A brand new post'
+	  	click_button 'Create Post'
+
+	  	expect(page).to have_content 'A brand new post'
+	  	expect(current_path).to eq '/posts'
+	  end
+
+	end
+
 end
