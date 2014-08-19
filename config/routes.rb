40fa04b84218cc68f04fe05b443b7adcf70a60e6
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-08-19 17:07:00 +0100

    Tags link to filter by tags

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :posts
+  resources :tags
+
+  root to: 'posts#index'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # Example of regular route:
--- spec/features/tagging_feature_spec.rb
+++ spec/features/tagging_feature_spec.rb
@@ -12,4 +12,20 @@ describe 'tagging' do
 		expect(page).to have_link '#pretty'
 	end
 
+	describe 'filter by tags' do
+		before do
+			Post.create(title: 'Post A', tag_list: '#birds, #pretty')
+			Post.create(title: 'Post B', tag_list: '#birds, #bob')
+		end
+
+		it 'filters to show only relevant tagged posts' do
+			visit '/posts'
+			click_link '#pretty'
+
+			expect(page).to have_css 'h1', text: 'Posts tagged with #pretty'
+			expect(page).to have_content 'Post A'
+			expect(page).not_to have_content 'Post B'
+		end
+	end
+
 end
